ad4885d895678b2cac23f824936622bdf698c3c7
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-11-28 15:51:41 -0500

    added components and navlinks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Home from './components/Home';
+import MyStory from './components/MyStory';
+import NavBar from './components/NavBar';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/story' element={<MyStory/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/MyStory.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MyStory() {
+    return (
+    <div>
+        <h1>My Story</h1>
+        <p>Taking the leap to become a Software Engineer</p>
+    </div>
+)
+}
